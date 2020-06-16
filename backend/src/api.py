3d860f70fbe2d58d4545db26bf0309126c6e09be
Finnvoid <neku113@gmail.com>
2020-06-15 22:23:50 -0400

    Finished To-Dos, Cleanup

--- backend/src/api.py
+++ backend/src/api.py
@@ -24,79 +24,82 @@ CORS(app)
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
-#db_drop_and_create_all()
+db_drop_and_create_all()
 
 # ===================
-## ROUTES
+# ROUTES
 # ===================
 
 # Get Drinks
 
+
 @app.route('/drinks')
 def retrieve_drinks():
-    
+
     drinks = Drink.query.all()
-    
-    try:
-        return jsonify({
-            'success': True,
-            'drinks' : [drink.short() for drink in drinks]
-        }), 200
-    except:
-        abort(404)
+
+    if len(drinks) == 0:
+        print("no drinks found")
+        abort(401)
+
+    drink_format = [drink.short() for drink in drinks]
+
+    return jsonify({
+        'success': True,
+        'drinks': drink_format
+    })
 
 # Get Drink-Detail
+
 
 @app.route('/drinks-detail')
 @requires_auth('get:drinks-detail')
 def get_drinks_detail(payload):
-    
-    drinks = Drink.query.all()
-    
-    try:
-        return jsonify({
-            'sucess': True,
-            'drinks': [drink.long() for drink in drinks]
-        }), 200
 
-    except:
+    drinks = Drink.query.all()
+
+    if len(drinks) == 0:
+        print("no drinks found")
         abort(404)
+
+    drink_format = [drink.long() for drink in drinks]
+
+    return jsonify({
+        'sucess': True,
+        'drinks': drink_format
+    })
 
 # Post Drink
 
+
 @app.route('/drinks', methods=['POST'])
 @requires_auth('post:drinks')
-def create_drink(payload, *args, **kwargs):
+def create_drink(payload):
 
     body = request.json
     new_drink = body['title']
     new_recipe = body['recipe']
 
-    try:
-        decoded_recipe = json.loads(new_recipe)
-    except json.JSONDecodeError:
-        abort(400)
+    drink = Drink(title=new_drink, recipe=json.dumps(new_recipe))
 
-    drink = Drink(title=new_drink, recipe=new_recipe)
-    
     drink.insert()
+
     return jsonify({
         "success": True,
         "drinks": drink.long()
     })
 
 
-        
 # Patch Drinks
 
 @app.route('/drinks/<id>', methods=['PATCH'])
 @requires_auth('patch:drinks')
 def edit_drink_by_id(*args, **kwargs):
-    
+
     body = request.get_json()
     id = kwargs['id']
     drink = Drink.query.filter_by(id=id).one_or_none()
-    
+
     if drink is None:
         abort(404)
 
@@ -119,7 +122,7 @@ def edit_drink_by_id(*args, **kwargs):
         'drinks': drink
     })
 
-    
+
 @app.route('/drinks/<id>', methods=['DELETE'])
 @requires_auth('delete:drinks')
 def delete_drink(payload, id):
@@ -136,7 +139,7 @@ def delete_drink(payload, id):
     return jsonify({
         'success': True,
         'delete': id
-    }),200
+    }), 200
 
 
 # =================================================================
@@ -146,15 +149,16 @@ def delete_drink(payload, id):
 @app.errorhandler(422)
 def unprocessable(error):
     return jsonify({
-        "success": False, 
+        "success": False,
         "error": 422,
         "message": "unprocessable"
     }), 422
 
+
 @app.errorhandler(404)
 def unreachable(error):
     return jsonify({
-        "success": False, 
+        "success": False,
         "error": 404,
         "message": "Not Found"
     }), 404
@@ -168,6 +172,7 @@ def bad_request(error):
         "message": "bad request"
     }), 400
 
+
 @app.errorhandler(405)
 def not_allowed(error):
     return jsonify({
@@ -177,7 +182,8 @@ def not_allowed(error):
     }), 405
 
 
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above 
-'''
+@app.errorhandler(AuthError)
+def auth_error(auth):
+    a_error = jsonify(auth.error)
+    a_error.status_code = auth.status_code
+    return a_error
